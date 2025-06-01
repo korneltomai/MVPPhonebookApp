f96d782bdc0b333cce856937de93d124e3f4443d
T3tr1s <tomai.kornelmunka@gmail.com>
2025-06-01 13:03:57 +0200

    Add: Unit test project

---
+++ WinFormsMVPPPhonebookApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WinFormsMVPPPhonebookApp.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
